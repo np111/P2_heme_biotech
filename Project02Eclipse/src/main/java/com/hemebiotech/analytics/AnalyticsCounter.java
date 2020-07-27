5c1b9e79e33fbc9591f8f3ab0bab517f371d19c6
np111 <oc.np111@protonmail.com>
2020-07-27 20:25:36 +0200

    Add SymptomsWriter

--- Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,11 +3,8 @@ package com.hemebiotech.analytics;
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 public class AnalyticsCounter {
     public static void main(String[] args) throws Exception {
@@ -19,17 +16,9 @@ public class AnalyticsCounter {
         counter.add(symptoms);
 
         // write symptoms.txt
-        try (Writer wr = new BufferedWriter(
-                new OutputStreamWriter(new FileOutputStream("result.out"), StandardCharsets.UTF_8))) {
-            // sort symptoms alphabetically
-            Iterator<Map.Entry<String, Integer>> it = counter.getCount().entrySet()
-                    .stream().sorted(Map.Entry.comparingByKey()).iterator();
-
-            // write line by line
-            while (it.hasNext()) {
-                Map.Entry<String, Integer> e = it.next();
-                wr.write(e.getKey() + ": " + e.getValue() + "\n");
-            }
+        try (SymptomsWriter wr = new SymptomsWriter(new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream("result.out"), StandardCharsets.UTF_8)))) {
+            wr.writeCount(counter.getCount());
         }
     }
 }
