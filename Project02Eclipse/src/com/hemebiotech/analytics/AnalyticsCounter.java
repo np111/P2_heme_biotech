f9ed9073ca0ae36a1fd451cfad0d613c12036139
np111 <oc.np111@protonmail.com>
2020-07-27 19:39:04 +0200

    Fix the alex work

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,43 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    public static void main(String[] args) throws Exception {
+        Map<String, Integer> symptomsCounts = new HashMap<>();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+        // read symptoms.txt line by line
+        try (BufferedReader rd = new BufferedReader(
+                new InputStreamReader(new FileInputStream("symptoms.txt"), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = rd.readLine()) != null) {
+                // count symptoms occurrences
+                symptomsCounts.merge(line, 1, Integer::sum);
+            }
+        }
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        // write symptoms.txt
+        try (Writer wr = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream("result.out"), StandardCharsets.UTF_8))) {
+            // sort symptoms alphabetically
+            Iterator<Map.Entry<String, Integer>> it = symptomsCounts.entrySet()
+                    .stream().sorted(Map.Entry.comparingByKey()).iterator();
+
+            // write line by line
+            while (it.hasNext()) {
+                Map.Entry<String, Integer> e = it.next();
+                wr.write(e.getKey() + ": " + e.getValue() + "\n");
+            }
+        }
+    }
 }
