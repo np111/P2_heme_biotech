113c90e458483d629cb716024d6d0337c3beb3af
np111 <oc.np111@protonmail.com>
2020-07-27 20:11:30 +0200

    Add SymptomsCounter

--- Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,24 +5,24 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 public class AnalyticsCounter {
     public static void main(String[] args) throws Exception {
-        Map<String, Integer> symptomsCounts = new HashMap<>();
-
-        // read symptoms.txt line by line
+        // read symptoms.txt
         List<String> symptoms = new FileSymptomReader("symptoms.txt").getSymptoms();
-        symptoms.forEach(symptom -> symptomsCounts.merge(symptom, 1, Integer::sum));
+
+        // count symptoms
+        SymptomsCounter counter = new SymptomsCounter();
+        counter.add(symptoms);
 
         // write symptoms.txt
         try (Writer wr = new BufferedWriter(
                 new OutputStreamWriter(new FileOutputStream("result.out"), StandardCharsets.UTF_8))) {
             // sort symptoms alphabetically
-            Iterator<Map.Entry<String, Integer>> it = symptomsCounts.entrySet()
+            Iterator<Map.Entry<String, Integer>> it = counter.getCount().entrySet()
                     .stream().sorted(Map.Entry.comparingByKey()).iterator();
 
             // write line by line
---
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/SymptomsCounter.java
@@ -0,0 +1,48 @@
+package com.hemebiotech.analytics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * An aggregator that counts the number of occurrences per symptom.
+ */
+public class SymptomsCounter {
+    private final Map<String, Integer> count = new HashMap<>();
+
+    /**
+     * Add a symptom occurrence.
+     *
+     * @param symptom the symptom to add
+     */
+    public void add(String symptom) {
+        Objects.requireNonNull(symptom, "symptom cannot be null");
+        count.merge(symptom, 1, Integer::sum);
+    }
+
+    /**
+     * Add many symptoms occurrences.
+     *
+     * @param symptoms the symptoms to add
+     */
+    public void add(Iterable<String> symptoms) {
+        symptoms.forEach(this::add);
+    }
+
+    /**
+     * Reset the current count.
+     */
+    public void reset() {
+        count.clear();
+    }
+
+    /**
+     * Return the current count.
+     *
+     * @return an unmodifiable map associating each symptom to is number of occurrences
+     */
+    public Map<String, Integer> getCount() {
+        return Collections.unmodifiableMap(count);
+    }
+}
