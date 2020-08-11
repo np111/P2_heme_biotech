0e8f5ec120fb40e67b2a02535f72f8e4762809a2
np111 <oc.np111@protonmail.com>
2020-08-11 18:44:46 +0200

    Declare variable by interface (and not implementation)

--- Project02Eclipse/src/main/java/com/hemebiotech/analytics/reader/InputStreamSymptomReader.java
+++ Project02Eclipse/src/main/java/com/hemebiotech/analytics/reader/InputStreamSymptomReader.java
@@ -25,7 +25,7 @@ public class InputStreamSymptomReader implements ISymptomReader {
 
     @Override
     public List<String> getSymptoms() throws IOException {
-        ArrayList<String> res = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
             String line;
             while ((line = rd.readLine()) != null) {
